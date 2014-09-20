3dc0ac287cabcff057a57f3c7ebc8a7a91fa9a74
kkaylium <kkaylium13@hotmail.com>
2014-09-19 21:59:49 -0600

    Starting Rainbow Gen

---
+++ source/net/kkaylium/mods/TechliumCraft/init/gen/structures/StructureRainbow.java
@@ -0,0 +1,18 @@
+package net.kkaylium.mods.TechliumCraft.init.gen.structures;
+
+import cpw.mods.fml.common.IWorldGenerator;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkProvider;
+
+import java.util.Random;
+
+/**
+ * Created by Kayla Marie on 9/19/14.
+ */
+public class StructureRainbow extends IWorldGenerator {
+
+    @Override
+    public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
+
+    }
+}
